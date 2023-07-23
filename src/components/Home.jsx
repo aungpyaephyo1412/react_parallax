94ad8e5ae67b996bec24900d0eeff59a431f40f9
typle445 <aungpyaephyo1412@gmail.com>
2023-07-23 17:48:21 +0630

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,11 @@
 import Hero from "./Partials/Hero.jsx";
-import About from "./About.jsx";
+
 
 const Home = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <Hero />
-      <About />
-      <About />
-      <About />
-      <About />
-      <About />
-      <About />
+      <div className="min-h-screen" />
     </div>
   );
 };
